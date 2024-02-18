aa5a8f441eddee0f34a84c396237918a0591c318
yahya-mohammed07 <goldroger300@gmail.com>
2024-02-18 22:45:49 +0300

    v0.0.2

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,23 +1,26 @@
+import { ReactNode } from "react";
 import React from "react";
-import "./modal.css";
 
 interface ModalType {
   isOpen: boolean;
   onClose: () => void;
-  children: React.ReactNode;
+  children: ReactNode;
 }
 
 const Modal: React.FC<ModalType> = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
+  // fade
   return (
-    <div onClick={onClose} className="overlay">
+    <div onClick={onClose} className="flex p-0 m-0 top-0 left-0 right-0 fixed w-full h-full justify-center items-center">
       <div
         onClick={(e) => {
           e.stopPropagation();
         }}
-        className="modalContainer"
+        className="flex max-w-xl fixed shadow-lg rounded-sm bg-white border-4 mx-auto"
       >
-        {children}
+        <div className="w-full">
+          <div className="flex flex-col text-center p-2">{children}</div>
+        </div>
       </div>
     </div>
   );
